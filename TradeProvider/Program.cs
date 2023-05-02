7f693324df6c81e7e9a8f0bfdfd4bf2f22b582be
Danil <131665618+defau1tusername@users.noreply.github.com>
2023-05-03 02:06:09 +0300

    Update Program.cs

--- TradeProvider/Program.cs
+++ TradeProvider/Program.cs
@@ -1,115 +1,46 @@
-﻿using ClosedXML.Excel;
-using DocumentFormat.OpenXml.EMMA;
-using DocumentFormat.OpenXml.ExtendedProperties;
-using DocumentFormat.OpenXml.Spreadsheet;
-using System.Net;
+using ClosedXML.Excel;
 
 public class TradeProvider
 {
     public static void Main()
     {
-        Console.Write("Введите путь до файла: ");
-        var path = Console.ReadLine();
-        var workbook = new XLWorkbook(path);
-        var clientDataProcessor = new ClientDataProcessor(new ExcelStorageInfoProvider(workbook));
-        
-        Console.WriteLine("Команды: \n - заказы \n - изменить контактное лицо \n - золотой клиент \n");
+        var workbook = GetWorkbook();
+        var excelStorageInfoProvider = new ExcelStorageInfoProvider(workbook);
+        var clientDataProcessor = new StorageInfoProcessor(excelStorageInfoProvider);
         while (true)
         {
+            Console.WriteLine("Команды: \n - заказы \n - изменить контактное лицо \n - золотой клиент \n");
+            Console.Write("Введите команду: ");
+            var command = Console.ReadLine();
             try
             {
-                Console.Write("Введите команду: ");
-                var command = Console.ReadLine();
-                ProcessCommand(command, clientDataProcessor);
+                CommandProvider.Execute(command, clientDataProcessor);
             }
-            catch (Exception exception) when (exception is CommandBaseException || exception is UnknownCommandException)
+            catch (Exception exception) 
+                when (exception is TradeProviderExceptionBase || exception is UnknownCommandException)
             {
                 Console.WriteLine(exception.Message);
             }
         }
     }
 
-    private static void ProcessCommand(string command, ClientDataProcessor clientDataProcessor)
+    private static XLWorkbook GetWorkbook()
     {
-        switch (command)
+        var workbook = (XLWorkbook)null;
+        while (workbook == null)
         {
-            case "заказы":
+            Console.WriteLine("Введите путь до файла: ");
+            var path = Console.ReadLine();
+            try
             {
-                ProcessOrderCommand(clientDataProcessor);
-                break;
+                workbook = new XLWorkbook(path);
             }
-            case "изменить контактное лицо":
+            catch (Exception)
             {
-                ProccessCustomerInfoCommand(clientDataProcessor);
-                break;
+                Console.WriteLine("Невероный путь до файла, введите еще раз:");
             }
-            case "золотой клиент":
-            {
-                ProcessGoldenClientCommand(clientDataProcessor);
-                break;
-            }
-            default:
-                throw new UnknownCommandException();
         }
-    }
 
-    private static void ProcessOrderCommand(ClientDataProcessor clientDataProcessor)
-    {
-        Console.Write("Введите наименование товара по которому провести поиск: ");
-        var product = Console.ReadLine();
-        var clientsAndOrders = clientDataProcessor.GetClientsAndOrders(product);
-        OutputClientsAndOrdersToConsole(clientsAndOrders);
+        return workbook;
     }
-    private static void ProccessCustomerInfoCommand(ClientDataProcessor clientDataProcessor)
-    {
-        Console.Write("Введите наименование организации у которой нужно изменить контактное лицо: ");
-        var company = Console.ReadLine();
-        Console.Write("Введите новое контактное лицо: ");
-        var contact = Console.ReadLine();
-        var clientUpdatedInfo = clientDataProcessor.ChangeClientInfo(company, contact);
-        OutputClientUpdatedInfo(clientUpdatedInfo);
-    }
-
-    private static void ProcessGoldenClientCommand(ClientDataProcessor clientDataProcessor)
-    {
-        Console.Write("Введите за какой период найти золотого клиента в формате mm/yyyy: ");
-        var date = Console.ReadLine();
-        var goldenClients = clientDataProcessor.GetGoldenClients(date);
-        OutputGoldenClientToConsole(goldenClients, date);
-    }
-
-    private static void OutputClientsAndOrdersToConsole(Dictionary<Client, ClientOrdersInfo> clientsAndOrders)
-    {
-        foreach (var clientAndOrders in clientsAndOrders)
-        {
-            Console.WriteLine("\n======================================= Информация о клиенте =======================================");
-            OutputClient(clientAndOrders.Key);
-            Console.WriteLine("======================================= Информация о заказах =======================================");
-            Console.WriteLine("Требуемое количество: " + clientAndOrders.Value.Count);
-            Console.WriteLine("Цена: " + clientAndOrders.Value.Price);
-            Console.WriteLine("Даты заказов: ");
-            foreach (var date in clientAndOrders.Value.Dates)
-                Console.WriteLine(" - " + date.ToString("dd/MM/yyyy"));
-            Console.WriteLine("=====================================================================================================\n");
-
-        }
-    }
-   
-    private static void OutputClientUpdatedInfo(Client clientUpdatedInfo)
-    {
-        Console.WriteLine("\n======================================= Обновленная информация: ======================================");
-        OutputClient(clientUpdatedInfo);
-        Console.WriteLine("======================================================================================================\n");
-    } 
-
-    private static void OutputGoldenClientToConsole(List<Client> goldenClients, string date)
-    {
-        Console.WriteLine($"\n================================= Золотые клиенты за период: {date} ================================");
-        foreach (var goldenClient in goldenClients)
-            OutputClient(goldenClient);
-        Console.WriteLine("=====================================================================================================\n");
-    }
-
-    private static void OutputClient(Client client) =>
-        Console.WriteLine($"{client.Code} | {client.Company} | {client.Address} | {client.Contact}");
 }
