15b6edb0ff3d8ec05c8631d29eccc8b39a56ad18
Danil <131665618+defau1tusername@users.noreply.github.com>
2023-05-04 03:09:11 +0300

    Add files via upload

---
+++ TradeProvider/Exceptions/InvalidDateException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InvalidDateException : TradeProviderExceptionBase
+{
+    public InvalidDateException()
+        : base("Формат даты не соответствует MM.yyyy")
+    { }
+}
+
---
+++ TradeProvider/Exceptions/SaveFailedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SaveFailedException : TradeProviderExceptionBase
+{
+    public SaveFailedException()
+        : base($"Ошибка при сохранении/изменении файла")
+    { }
+}
+
